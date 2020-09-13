21b8d5eeb8e443fbe9263efc211d773bd31d886a
XV1R <xavytron@gmail.com>
2020-09-13 16:16:53 -0400

    restructured project; model now is trained and generates proper log file, as well as opens up graph

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import fileutils, darknetutils, cvutils
+
 import os,sys
-sys.path.append("./images")
+sys.path.append("./images/")
+sys.path.append("./model/")
+sys.path.append("./src/")
+from src import fileutils, darknetutils, cvutils
+
 
 
 data = fileutils.files2list( "./images", ".jpg" )
@@ -8,17 +12,25 @@ data = fileutils.files2list( "./images", ".jpg" )
 #saves us a for loop
 paths = list(map(lambda x: "./images/"+str(x),data))
 
-#writes the training data into the model folder 
-os.chdir("./model/")
+#writes the training data 
 with open("train.txt","w") as trainFile:
     for image in paths:
         trainFile.write(image)
         trainFile.write("\n")
     trainFile.close()
-os.chdir("..")
+
+
+#test file 
+test_lines = open("train.txt","r").readlines()
+with open("test.txt","w") as testFile:
+    for line in test_lines:
+        testFile.write(line)
+    testFile.close()
+
 
 #My darknet installation is in /opt/
 #Use the path to call darknet from this folder 
-#NOTE: currently only generates a log file
+#NOTE: currently generates log file and is able to open graph
+#NOTE: The custom.name file is copied into the overall folder; couldn't get the script to find it 
 darknetutils.darknet_pretrain("/opt/darknet/darknet","./model/yolov3-tiny-generated.cfg","yolov3-tiny.weights")
 darknetutils.darknet_train("/opt/darknet/darknet", "./model/detector.data", "./model/yolov3-tiny-generated.cfg","pretrained-weights.conv.15")
